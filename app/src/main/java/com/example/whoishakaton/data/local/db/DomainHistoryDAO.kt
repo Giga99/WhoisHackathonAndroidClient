a9c39d4096ad2a5701cc4b262bacfad4b6b8d170
Igor Stevanovic <igorstevanovic99@gmail.com>
2021-09-25 10:58:46 +0200

    Added DomainHistoryDAO base functions

--- app/src/main/java/com/example/whoishakaton/data/local/db/DomainHistoryDAO.kt
+++ app/src/main/java/com/example/whoishakaton/data/local/db/DomainHistoryDAO.kt
@@ -1,7 +1,17 @@
 package com.example.whoishakaton.data.local.db
 
-import androidx.room.Dao
+import androidx.room.*
+import com.example.whoishakaton.data.local.db.entities.DomainHistoryEntity
 
 @Dao
 interface DomainHistoryDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(domain: DomainHistoryEntity): Long
+
+    @Delete()
+    suspend fun delete(domain: DomainHistoryEntity)
+
+    @Query("SELECT * FROM DOMAIN_HISTORY_TABLE")
+    suspend fun getAllSearchedDomains(): List<DomainHistoryEntity>
 }
